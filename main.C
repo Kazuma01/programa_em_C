d8266eae90def736764b79929b9e6a3f966194c5
Kazuma01 <100470773+Kazuma01@users.noreply.github.com>
2024-07-19 01:39:48 -0300

    Update main.C

--- main.C
+++ main.C
@@ -1,1 +1,24 @@
+#include<stdio.h>
+
+    float a,b;
+
+    int somar(){
+        return (a+b);
+    }
+
+int main()
+{
+    float resposta;
+
+    printf("\nDigite o primeiro numero");
+    scanf("%f",&a);
+    printf("\n");
+    printf("\nDigite o segundo numero");
+    scanf("%f",&b);
+    printf("\n");
+    resposta = somar();
+    printf("a soma é %.0f",resposta);
+
+    return 0;
+}
 
